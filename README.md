717446cf2492b01425f5dc8be39287b9b3f64760
joacogm12 <gomezjoaquin991@gmail.com>
2022-07-11 21:41:39 -0500

    java script documentation, readme file and small changes to the api calls

---
+++ README.md
@@ -0,0 +1,41 @@
+# Weather App
+
+## Project Description
+* This weather app tells the user the current weather and the forecast for five days of a city of their selection. It was made using the openweatheramp api to get all the information
+
+* link to the api: https://openweathermap.org/api
+
+* I did my best making this application if there is anyquestions please ask me or if ther is anything you think i can improve please let me know 
+
+* The aplication is not responsive but i will make it in a future commit
+
+## technologies
+
+* html
+
+* css
+
+* javascript
+
+## How to use it
+this is the application it is user friendly 
+
+![image1](Assets/images/Captura%20de%20pantalla%202022-07-11%20211408.png)
+
+
+In this part of the weather app the current weather of the choosen city will be displayed
+
+![image1](Assets/images/currentDay.png)
+
+
+Here the use can submit the city and use the buttons to diplay the weather of cities the user has searched  
+
+![image1](Assets/images/search%20history.png)
+
+
+And last but not least the forecast for 5 days
+![image1](Assets/images/forecast.png)
+
+## Deployed Application
+
+Link to the deployed project: https://joacogm12.github.io/weather-app/
--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-
+//containers for elements created from js
 const cityWeatherEl = $(".city-weather");
 const forecastEl = $("#forecast");
 const optionsEl = $(".options");
 
+//variables to conect to the api
 var APIKey = "2266e4f4412f39e387b59d7903274cff";
-var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
+var url = "HTTPs://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
 
+//get elements from local storage
 var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr")) || [];
+
+//name of the city
 var city = "";
+
+//amount of buttons for the search history
 var btnCount = 0;
 
+//variable to make some validations
 var check = 0;
 
-
+//this function creates all the initial elements of the html
 function createElements() {
+
+    //in here the elements for the current weather are created
     cityWeatherEl.append('<h1 id="cityName"></h1>');
     cityWeatherEl.append('<div id="wathertoday"></div>');
     const weatherTodayEl = $("#wathertoday");
@@ -25,12 +34,13 @@ function createElements() {
     weatherTodayEl.append("<p> Humidity: </p> <p id='currentHum'></p>");
     weatherTodayEl.append("<p> UV index: </p> <p id='currentUv'></p>");
 
+    //in here the elements for the 5 days of forecast are created
     for (let i = 0; i < 5; i++) {
         forecastEl.append("<div id='forecastday" + i + "'></div>");
         const forecast = $("#forecastday" + i + "");
 
         forecast.append("<h2 id='date" + i + "'></h2>");
-        forecast.append("<img id='icon" + i + "' src='Assets/images/cloudy.png' alt='weather-icon'>")
+        forecast.append("<img id='icon" + i + "'alt='weather-icon'>")
         forecast.append("<p>max temp:</p> <p id='tempMax" + i + "'></p>");
         forecast.append("<p>min temp:</p> <p id='tempMin" + i + "'></p>");
         forecast.append("<p>wind:</p> <p id='wind" + i + "'></p>");
@@ -39,6 +49,7 @@ function createElements() {
         forecastEl.append(forecast);
     }
 
+    //in here the buttons are created, the amount of buttons depend on the amount of elements saved in local storage
     for (let i = 0; i < searchHistoryArr.length; i++) {
         btnCount = i;
         optionsEl.append("<button class='city-options' onclick = 'btnClick(this);' value='" + searchHistoryArr[btnCount].city + "'>" + searchHistoryArr[btnCount].city + "</button>")
@@ -46,19 +57,22 @@ function createElements() {
     optionsEl.append("<button class='clear' onclick='clearHistory()'> clear history </button>")
 }
 
-
+//this function is called when the form in which the user makes the input is submited
 $("#inputCity").on("submit", function apiCall(e) {
     e.preventDefault();
 
+    //
     if (check == 0) {
         city = $('input[name="city-input"]').val();
     }
 
-    url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
+    //first api call to get the lat and lon of the city inputed
+    url = "HTTPs://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
     $.ajax({
         url: url,
         method: "GET",
     }).then(function (response) {
+        // calls another function that makes another api call with the lat and lon
         renderValues(response.coord.lat, response.coord.lon, response.name);
     });
 
@@ -68,12 +82,14 @@ $("#inputCity").on("submit", function apiCall(e) {
 
 
 function renderValues(lat, lon, name) {
-    var urlforecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric"
+    //api call that contains more information 
+    var urlforecast = "HTTPs://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric"
     $.ajax({
         url: urlforecast,
         method: "GET"
     }).then(function (response) {
 
+        //values for the current weather are inputed into the elements of the html
         console.log(response)
         $("#cityName").html(name);
         $("#currentDate").html(getDatePrint(response.daily[0].dt));
@@ -82,21 +98,19 @@ function renderValues(lat, lon, name) {
         $("#currentHum").html(response.current.humidity + " %");
         $("#currentUv").html(response.current.uvi);
 
+        //values of the 5 day forecast are inputed into the elements of the html
         for (let i = 0; i < 5; i++) {
-
             var iconCode = response.daily[i + 1].weather[0].icon;
-            var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            var iconurl = "HTTPs://openweathermap.org/img/w/" + iconCode + ".png";
             $("#icon" + i).attr('src', iconurl);
             $("#date" + i).html(getDatePrint(response.daily[i + 1].dt));
             $("#tempMax" + i).html(Math.round(response.daily[i + 1].temp.max) + "°C");
             $("#tempMin" + i).html(Math.round(response.daily[i + 1].temp.min) + "°C");
             $("#wind" + i).html(Math.round(response.daily[i + 1].wind_speed) + "mph");
             $("#humidity" + i).html(Math.round(response.daily[i + 1].humidity) + "%");
-
         }
 
     });
-
 
     searchHistory(lat, lon)
     check = 0;
@@ -104,7 +118,7 @@ function renderValues(lat, lon, name) {
 
 }
 
-
+//this function transforms the date given by the api into an ctual readable date
 function getDatePrint(dailyForecast) {
 
     var month = 0;
@@ -133,21 +147,26 @@ function getDatePrint(dailyForecast) {
     return actualDate;
 }
 
-
+//saves values into local storage
 function searchHistory(lat, lon) {
+    //id for each city
     var id = Math.round((lat + lon) * 100) / 100;
+
+    //flag to make validations
     var flag = true;
 
     searchHistoryArr.reverse();
 
+    //object that saves the name of the city and it´s id
     cityObj = {
         city: city,
         id: id
     }
 
+    //checks if the array already has the value that was inputed by the user 
+    //returns true if the value is new and false if there is an equal value
     for (let i = 0; i < searchHistoryArr.length; i++) {
         if (searchHistoryArr[i].id == id) {
-            console.log("false");
             flag = false;
             break
         } else {
@@ -155,6 +174,7 @@ function searchHistory(lat, lon) {
         }
     }
 
+    //if the validation was true cityobj gets pushed into the array
     if (flag == true) {
         searchHistoryArr.push(cityObj);
         btnCount++
@@ -162,28 +182,29 @@ function searchHistory(lat, lon) {
     }
 
     searchHistoryArr.reverse();
+
+    //the array gets saved into local storage
     localStorage.setItem("searchHistoryArr", JSON.stringify(searchHistoryArr));
 }
 
-
-window.onload = historyCity();
-
-
+//this function is called when the page is loaded
 function historyCity() {
+    //validates if there is anything in local storage
+    //if there is nothing in local storage it will input the city san luis potosi
+    //if ther is something in local storage it will input the first element of local storage 
     if (searchHistoryArr.length == 0) {
-
         city = "san luis potosi";
         check = 1;
         $("#inputCity").submit();
     } else {
-
         city = searchHistoryArr[0].city;
         check = 1;
         $("#inputCity").submit();
     }
 }
 
-
+//this function is called by the search history buttons
+//inputs the value of the buttons 
 function btnClick(e) {
     city = e.value;
     console.log(city)
@@ -191,10 +212,11 @@ function btnClick(e) {
     $("#inputCity").submit();
 }
 
-
+//clears local storage to clear history
 function clearHistory() {
     localStorage.clear();
     location.reload();
 }
 
+window.onload = historyCity();
 createElements();
